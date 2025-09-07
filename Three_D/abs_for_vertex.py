c4c131424f34ddc316146202160ff7a8a6c58622
Duncan Holmes <dholmes4@huskers.unl.edu>
2025-09-06 21:14:42 -0500

    final commit of the night. the next few days where i see 1.4. i will be crying like a chimp

--- Three_D/abs_for_vertex.py
+++ Three_D/abs_for_vertex.py
@@ -8,3 +8,7 @@ def abs_for_vertex(vertex,tab_amount):
     output_num = math.pow(vertex[0],2) + math.pow(vertex[1],2) + math.pow(vertex[2],2)
     print(f"{tab_amount}\t{math.sqrt(output_num)}")
     return output_num
+
+if __name__ == "__main__":
+    vertex_var = [-4,1,-10]
+    print( abs_for_vertex(vertex=vertex_var,tab_amount="\t"))
