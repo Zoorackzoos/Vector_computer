6b23a7fbada8d61373664cc53a6faa7e66639678
Duncan Holmes <dholmes4@huskers.unl.edu>
2025-09-22 21:58:18 -0500

    minor change

--- math_only/Three_D/ABS_for_vertex_aka_distance_formula.py
+++ math_only/Three_D/ABS_for_vertex_aka_distance_formula.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     vertex_var__1_4_4 = [2,-2,-4]
     vertex_var__1_4_7_force = [0,5,2]
     vertex_var__1_4_8 = [9,0,-9]
+    vertex_var__2_5_3 = [12,-4,6]
 
-    vertex_in_question = vertex_var__1_4_8
+    vertex_in_question = vertex_var__2_5_3
 
     print(ABS_for_vectors_AKA_distance_formula(vertex=vertex_in_question, tab_amount="\t"))
