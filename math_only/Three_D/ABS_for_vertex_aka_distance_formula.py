85d15c1f8f11eab1fede0e4fefcf3cd1827f51d0
Duncan Holmes <dholmes4@huskers.unl.edu>
2025-10-07 16:04:17 -0500

    idk lol. check loser

--- math_only/Three_D/ABS_for_vertex_aka_distance_formula.py
+++ math_only/Three_D/ABS_for_vertex_aka_distance_formula.py
@@ -1,5 +1,7 @@
 import math
 
+from print_list import print_list
+
 
 def ABS_for_vectors_AKA_distance_formula(vertex,tab_amount):
     """
@@ -30,6 +32,24 @@ if __name__ == "__main__":
     vertex_var__2_5_4 = [1,1,1]
     vertex_var__2_5_7 = [-1,-2,-1]
 
-    vertex_in_question = vertex_var__2_5_7
+    vertex_var__review_6_v = [3,1,4]
+    vertex_var__review_6_w = [2,-2,1]
 
-    print(ABS_for_vectors_AKA_distance_formula(vertex=vertex_in_question, tab_amount="\t"))
+    vertex_var__review_7_v = [2,3,6]
+    vertex_var__review_7_w = [1,-1,1]
+
+    vertex_var__review_8_v = [2, -6, 9]
+    vertex_var__review_8_w = [-1,1,-1]
+
+    vertex_var_review_13_v = [2,-2,1]
+    vertex_var_review_13_w = [4,0,-3]
+
+    vertex_v_in_question = vertex_var_review_13_v
+    vertex_w_in_question = vertex_var_review_13_w
+
+    output_list = []
+
+    output_list.append( ABS_for_vectors_AKA_distance_formula(vertex=vertex_v_in_question,tab_amount="\t") )
+    output_list.append( ABS_for_vectors_AKA_distance_formula(vertex=vertex_w_in_question,tab_amount="\t") )
+
+    print_list(list=output_list, tab_amount="")
