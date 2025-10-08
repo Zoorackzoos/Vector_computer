1b44a21eb71936e207c6b6b0d1a0f70905d9aae7
Duncan Holmes <dholmes4@huskers.unl.edu>
2025-10-08 12:11:08 -0500

    combonation exhuaster. dunno if i'll finish it.

---
+++ combonation_exhauster.py
@@ -0,0 +1,9 @@
+def combination_exhauster(array, tab_amount):
+    print(f"{tab_amount}combination_exhauster")
+
+
+
+if __name__ == "__main__":
+    array = ["a","b","c","d"]
+
+    combination_exhauster(array, tab_amount="")
